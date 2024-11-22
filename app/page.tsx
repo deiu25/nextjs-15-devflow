b738854836e87024cfd81e997e3da8fec7d5ad61
Deiu <d3iu25@gmail.com>
2024-11-22 17:13:06 +0200

    prettier and eslint settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 const Home = () => (
   <>
-  <h1 className="h1-bold">Welcome to the world of Next.Js</h1>
-  <h1 className="h1-bold font-space-grotesk">Welcome to the world of Next.Js</h1>
+    <h1 className="h1-bold">Welcome to the world of Next.Js</h1>
+    <h1 className="h1-bold font-space-grotesk">
+      Welcome to the world of Next.Js
+    </h1>
   </>
 );
 
