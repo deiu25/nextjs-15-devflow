548949c6baefd9a99ad16ad17fedd71703f522fe
Deiu <d3iu25@gmail.com>
2024-11-25 15:07:34 +0200

    setup dark and light theme

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import Theme from './Theme'
 
 const Navbar = () => {
     return (
@@ -21,7 +22,7 @@ const Navbar = () => {
             <p>Global Search</p>
 
             <div className="flex-between gap-5">
-                Theme
+                <Theme />
             </div>
         </nav>
     )
