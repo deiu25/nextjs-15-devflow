048c4001c81fdcc61fa0ba777a5e9b6e9319025f
Deiu <d3iu25@gmail.com>
2024-11-29 12:23:38 +0200

    home layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,24 +1,29 @@
-import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
+import Link from "next/link";
+
 
 const Home = async () => {
-  const session = await auth();
-
-  console.log(session);
-
   return (
     <>
-      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+      <section className="flex w-full flex-col-reverse sm:flex-row justify-between gap-4 sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
 
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
+      <section className="mt-11">
+        LocalSearch
+      </section>
+      HomeFilter
 
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-      </form>
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+      </div>
     </>
   );
 };
