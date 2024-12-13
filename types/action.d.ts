32b1291ce2bda8329926a27db5a8373801d9d704
Badulescu Andrei <d3iu25@gmail.com>
2024-12-13 14:03:32 +0200

    implement edit and edit server actions

--- types/action.d.ts
+++ types/action.d.ts
@@ -2,22 +2,30 @@ interface SignInWithOAuthParams {
     provider: "github" | "google";
     providerAccountId: string;
     user: {
-        email: string;
-        name: string;
-        image: string;
-        username: string;
+      email: string;
+      name: string;
+      image: string;
+      username: string;
     };
-}
-
-interface AuthCredentials {
+  }
+  
+  interface AuthCredentials {
     name: string;
     username: string;
     email: string;
     password: string;
-}
-
-interface CreateQuestionParams {
+  }
+  
+  interface CreateQuestionParams {
     title: string;
     content: string;
     tags: string[];
-}
+  }
+  
+  interface EditQuestionParams extends CreateQuestionParams {
+    questionId: string;
+  }
+  
+  interface GetQuestionParams {
+    questionId: string;
+  }
